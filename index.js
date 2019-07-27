24aaceaee40cdff60a49a999f21fa6f5c1da3019
乔 <qiao@lemonbox.me>
2019-07-27 21:32:10 +0800

    双线程录制音视频

--- index.js
+++ index.js
@@ -14,26 +14,32 @@ app.use(function (req, res, next) {
 process.on('uncaughtException', function (err) {
     log.error('未知异常', err)
 });
-let option1 = `ffmpeg -f avfoundation -framerate 30 -video_size 1280x720 -i "0:0" \
--codec:v mpeg1video -s 640x360 -r 30 -b:v 1500k -bf 0 \
--codec:a mp2 -b:a 128k -f mpegts http://127.0.0.1:8081/stream`
-//音视频聊天
-let option2 = `ffmpeg -f avfoundation -framerate 30 -video_size 1280x720 -i "1:0" \
+const option1 = `ffmpeg -f avfoundation -framerate 30 -video_size 1280x720 -i "0:none" \
+-codec:v mpeg1video -s 640x360 -r 30 -b:v 1500k -bf 0 -muxdelay 0.001 \
+-f mpegts http://127.0.0.1:8081/stream`
+//视频聊天
+// ffmpeg -f avfoundation "none:0" -codec:a mp2 -b:a 128k -muxdelay 0.001 -f mpegts http://127.0.0.1:8081/stream
+const option2 = `ffmpeg -f avfoundation -framerate 30 -video_size 1280x720 -i "1:0" \
 -codec:v mpeg1video -s 640x360 -r 30 -b:v 1500k -bf 0 \
 -codec:a mp2 -b:a 128k -f mpegts http://127.0.0.1:8081/stream`
 //录屏及录音聊天
-let childProcess;
+const soption = `ffmpeg -f avfoundation -i "none:0" \
+-codec:a mp2 -b:a 128k -muxdelay 0.001 -f mpegts http://127.0.0.1:8081/stream`
+//音频跟视频一起录制 音频出现较大的延迟，因此决定使用双线程分别采集音频和视频数据。效果相当明显。
+let childProcess1,childProcess2;
 app.get('/', function(req, res) {
-    if(req.query.run==='1'&&!childProcess){
-        ffmpegStreamStart(option1);
+    if(req.query.run==='1'&&!childProcess1&&!childProcess2){
+        ffmpegStreamStart(option1,soption);
         res.send(JSON.stringify({status: 'running'}));
-    } else if(req.query.run==='2'&&!childProcess){
-        ffmpegStreamStart(option2);
+    } else if(req.query.run==='2'&&!childProcess1&&!childProcess2){
+        ffmpegStreamStart(option2,soption);
         res.send(JSON.stringify({status: 'running'}));
     } else{
-        if(childProcess){
-            childProcess.kill();
-            childProcess = undefined;
+        if(childProcess1||childProcess2){
+            childProcess1.kill();
+            childProcess2.kill();
+            childProcess1 = undefined;
+            childProcess2 = undefined;
             res.send(JSON.stringify({status: 'stoped'}));
             return;
         }
@@ -52,8 +58,11 @@ console.log(
   `Server is listening on http://${process.env.HOST}:${process.env.PORT}`
 )
 
-function ffmpegStreamStart(option){
-    childProcess = exec(option , function(err, stdout,stderr){
+function ffmpegStreamStart(option,soption){
+    childProcess1 = exec(option , function(err, stdout,stderr){
+        console.log('error is ',err,'\nstdout is ',stdout,'\n stderr is ',stderr);
+    });
+    childProcess2 = exec(soption , function(err, stdout,stderr){
         console.log('error is ',err,'\nstdout is ',stdout,'\n stderr is ',stderr);
     });
 }
